a42e8d048f037b1b0ca642ea89d011c12f2f3e13
neilmillard-msm <neil.millard@moneysupermarket.com>
2016-10-11 17:08:48 +0100

    update replset

--- puppet/modules/role/manifests/mongodb.pp
+++ puppet/modules/role/manifests/mongodb.pp
@@ -11,11 +11,11 @@ class role::mongodb {
   }
 
   include ::profile::mongodb1
-  include ::profile::mongodb_repl_set_wt
+  #include ::profile::mongodb_repl_set_wt
   #include ::profile::mongodb_wt_backup
 
   package { 'mongodb-org-tools': ensure => installed, require => Yumrepo['mongodb'] }
 
      Class['profile::mongodb1']
-  -> Class['profile::mongodb_repl_set_wt']
+  #-> Class['profile::mongodb_repl_set_wt']
 }
