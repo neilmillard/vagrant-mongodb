78a8c58823fccc9fa91e63b731cf656108d075b8
neilmillard-msm <neil.millard@moneysupermarket.com>
2016-10-12 12:24:12 +0100

    need user

--- puppet/modules/role/manifests/mongodb.pp
+++ puppet/modules/role/manifests/mongodb.pp
@@ -10,14 +10,16 @@ class role::mongodb {
     stage => swapfile
   }
 
-  $datapath = hiera('mongodb::server::dbpath','/mnt/data')
+  $datapath = hiera('mongodb::server::dbpath','/data/db')
   $datauser = hiera('mongodb::server::user','mongod')
   $datagrp  = hiera('mongodb::server::grp','mongod')
+  $serverpkg = hiera('mongodb::server::package_name', 'mongodb-org-server')
   file {"${datapath}":
     ensure => directory,
     mode   => '0755',
     owner => "${datauser}",
     group => "${datagrp}",
+    require => Package["${serverpkg}"],
     before => Service['mongodb']
   }
 
