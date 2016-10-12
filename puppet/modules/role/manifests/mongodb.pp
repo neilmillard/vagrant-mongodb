e993938698e32f25050b1c2e026a1e6d55fc7f11
neilmillard-msm <neil.millard@moneysupermarket.com>
2016-10-12 11:56:55 +0100

    check data folder

--- puppet/modules/role/manifests/mongodb.pp
+++ puppet/modules/role/manifests/mongodb.pp
@@ -10,12 +10,24 @@ class role::mongodb {
     stage => swapfile
   }
 
+  $datapath = hiera('mongodb::server::dbpath','/mnt/data')
+  $datauser = hiera('mongodb::server::user','mongod')
+  $datagrp  = hiera('mongodb::server::grp','mongod')
+  file {"${datapath}":
+    ensure => directory,
+    mode   => '0755',
+    owner => "${datauser}",
+    group => "${datagrp}",
+    before => Service['mongodb']
+  }
+
   include ::profile::mongodb1
   #include ::profile::mongodb_repl_set_wt
   #include ::profile::mongodb_wt_backup
 
   package { 'mongodb-org-tools': ensure => installed, require => Yumrepo['mongodb'] }
 
-  #   Class['profile::mongodb1']
+
+  # Class['profile::mongodb1']
   #-> Class['profile::mongodb_repl_set_wt']
 }
