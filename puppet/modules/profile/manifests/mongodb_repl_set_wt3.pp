991d4c2eae4b04510bd86dee1a2164755d8d9064
neilmillard-msm <neil.millard@moneysupermarket.com>
2016-10-20 14:13:42 +0100

    update for wt3

---
+++ puppet/modules/profile/manifests/mongodb_repl_set_wt3.pp
@@ -0,0 +1,72 @@
+# the mongodb replica set for the 'role service-pool1-mongodb-wt'
+class profile::mongodb_repl_set_wt3 {
+
+    $repl_domain          = hiera('profile::mongodb_replset::domain')
+    $mongodb_replset_name = hiera('profile::mongodb_replset::mongodb_replset_name')
+    $replset_members      = hiera('profile::mongodb_replset::members')
+    $num_instances        = hiera('profile::mongodb_replset::num_instances', '2')
+    $db_path              = hiera('mongodb::server::dbpath')
+    $tls_pki_path         = hiera('profile::mongodb_replset::pki_path', '/etc/pki/mongodb')
+    $tls_chl_pass         = hiera('ife_toolbelt::challenge_password','chanGEm3')
+    $ssl_mode             = hiera('profile::mongodb::sslmode', 'preferSSL')
+    $db_auth              = hiera('profile::mongodb::auth', false)
+    # skydns config
+    $etcd_host     = hiera('mongodb::skydns::etcd_host', 'etcd.internal')
+    $etcd_port     = hiera('mongodb::skydns::etcd_port', '4001')
+    $record_port   = hiera('mongodb::skydns::record_port', '27017')
+    $record_ttl    = hiera('mongodb::skydns::record_ttl', '360')
+
+
+  include ::mongodb::globals
+  include ::mongodb::client
+
+  file {'/etc/facter/facts.d/init_fhn.txt':
+    content => "init_fhn=${repl_domain}",
+    owner   => 'sensu',
+    group   => 'sensu',
+    mode    => '0644',
+    require => Exec['install_sensu'],
+  }
+
+  Class['mongodb::globals']
+  ->
+  Class['mongodb::server']
+  ->
+  Class['mongodb::client']
+  # ->
+  # mongodb_replset{"${mongodb_replset_name}":
+  #   members => "${replset_members}"
+  # }
+
+
+  package {'nc':
+      ensure  => installed,
+    }
+    ->
+  file { '/usr/local/bin/mongodb_replset3.sh':
+    ensure  => present,
+    mode    => '0755',
+    owner   => root,
+    group   => root,
+    content => template('profile/mongodb/mongodb_replset_wt3.sh.erb'),
+    require => [ Class['::mountdevice'], Class['profile::skydns_client'], File[$tls_pki_path], File['/etc/facter/facts.d/init_fhn.txt'], Class['mongodb::server'] ],
+  }
+  ->
+  exec {'ensure_permissions_correct_wt3':
+    command => '/bin/chmod -R u+x /var/lib/mongo'
+  }
+  ->
+  exec { '/usr/local/bin/mongodb_replset3.sh':
+    command     => '/usr/local/bin/mongodb_replset3.sh',
+    provider    => shell,
+    timeout     => 0,
+    require     => [ File['/usr/local/bin/mongodb_replset3.sh'], Service['mongod'] ],
+    environment => [
+      "PKI_PATH=${tls_pki_path}",
+      "PEM_PASS=${tls_chl_pass}",
+      "SSL_MODE=${ssl_mode}",
+      "USE_DB_AUTH=${db_auth}",
+      "NUM_INSTANCES=${num_instances}"
+    ],
+  }
+}
--- puppet/modules/role/manifests/mongodb.pp
+++ puppet/modules/role/manifests/mongodb.pp
@@ -11,20 +11,14 @@ class role::mongodb {
     stage => swapfile
   }
 
-  # from puppetlabs-mongodb/manifests/params.pp
-  $pidfilepath  = '/var/run/mongodb/mongod.pid'
-
   $datapath     = hiera('mongodb::server::dbpath','/data/db')
-  $datauser     = hiera('mongodb::server::user','mongod')
-  $datagrp      = hiera('mongodb::server::grp','mongod')
-  $serverpkg    = hiera('mongodb::server::package_name', 'mongodb-org-server')
 
   exec { "create_datafolder":
     command => "/usr/bin/mkdir -p ${datapath}",
   }
 
-  include ::profile::mongodb1
-  #include ::profile::mongodb_repl_set_wt
+  #include ::profile::mongodb1
+  include ::profile::mongodb_repl_set_wt3
   #include ::profile::mongodb_wt_backup
 
   package { 'mongodb-org-tools': ensure => installed, require => Yumrepo['mongodb'] }
